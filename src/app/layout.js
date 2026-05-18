f023330452697a29b1c6f730b91345fc69e99e93
Moajjem Hossain <moajjem441@gmail.com>
2026-05-18 11:25:11 +0600

    dependencies installing

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ThemeProvider from "@/providers/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,8 +22,12 @@ export default function RootLayout({ children }) {
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+     suppressHydrationWarning>
+      <body className="min-h-full flex flex-col bg-background text-foreground">
+       <ThemeProvider>
+         {children}
+       </ThemeProvider>
+        </body>
     </html>
   );
 }
