ceea0235ea3b1094021c5514d762139a5c1da627
Moajjem Hossain <moajjem441@gmail.com>
2026-05-24 20:34:16 +0600

    code updated

--- src/proxy.js
+++ src/proxy.js
@@ -19,5 +19,5 @@ return NextResponse.redirect(new URL('/login', request.url))
 
  
 export const config = {
-  matcher: ['/trending_ideas/:path','/ideas/:path','/add-ideas','/my-idea','/profile','/my-interactions'],
+  matcher: ['/trending_ideas/:path','/ideas/:path','/add-ideas','/my-ideas','/profile','/my-interactions'],
 }
